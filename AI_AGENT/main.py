3906a5cbf4606733216fd07332db9e44cf1b3efd
Drei Abmab <dreiabmab@gmail.com>
2025-09-04 19:55:38 +0800

    AI agent can talk

--- AI_AGENT/main.py
+++ AI_AGENT/main.py
@@ -1,6 +1,65 @@
-def main():
-    print("Hello from ai-agent!")
+import os
+import json
+from typing import TypedDict
+
+from dotenv import load_dotenv
+from imap_client import IMAPClient
+
+from imap_tools import MailBox, AND
+
+from langchain.chat_models import init_chat_model
+from langchain_core.tools import tool
+
+from langgraph.prebuilt import ToolNode
+from langgraph.graph import StateGraph, START, END
+
+load_dotenv()
+
+IMAP_HOST = os.getenv("IMAP_HOST")
+IMAP_PORT = os.getenv("IMAP_PORT")
+IMAP_USER = os.getenv("IMAP_USER")
+IMAP_PASSWORD = os.getenv("IMAP_PASSWORD")
+IMAP_FOLDER = 'INBOX'
+
+CHAT_MODEL = "qwen3:8b"
+
+class ChatState(TypedDict):
+    messages: list
 
 
-if __name__ == "__main__":
-    main()
+def connect():
+    mail_box = MailBox(IMAP_HOST)
+    mail_box.login(IMAP_USER, IMAP_PASSWORD, initial_folder=IMAP_FOLDER)
+
+    return mail_box
+
+
+@tool
+def list_unread_emails():
+    """Return a bullet list of every UNREAD message's UID, subject date and sender """
+
+    print("List Unread Email Tool Called")
+
+    with connect() as mb:
+        unread = list(mb.fetch(criteria = AND(seen=False), headers_only=True, mark_seen=False))
+
+    if not unread:
+        return "You have no unread messages"
+
+    response = json.dumps([
+        {
+            'uid': mail.uid,
+            'date': mail.date.astimezone().strftime('%Y-%m-%d %H:%M'),
+            'subject': mail.subject,
+            'sender': mail.from_,
+
+        } for mail in unread
+    ])
+
+    return response
+
+
+def summarize_email(uid):
+    """Summarize an e-mail given its IMAP UID. Return a short summary of the e-mail content / body in plain text."""
+
+    
