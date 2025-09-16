0ec4616f15ca17c98d99f38ec171db115c467eb3
Drei Abmab <dreiabmab@gmail.com>
2025-09-16 22:51:05 +0800

    render frames var

--- Facial_Landscape/main.py
+++ Facial_Landscape/main.py
@@ -9,3 +9,10 @@ face_mesh = mp_face_mesh.FaceMesh( static_image_mode=False, max_num_faces = 3 )
 # Drawing utils
 map_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
+
+# Start webcam capture
+cap = cv2.VideoCapture(0)
+
+while cap.isOpened():
+    success, frame = cap.read()
+    
