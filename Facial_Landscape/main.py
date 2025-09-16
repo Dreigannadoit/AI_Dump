0cd2a98a5863582c8af8cb64c670d9f8e040c1d3
Drei Abmab <dreiabmab@gmail.com>
2025-09-16 22:49:03 +0800

    draw landmark var

--- Facial_Landscape/main.py
+++ Facial_Landscape/main.py
@@ -5,3 +5,7 @@ import mediapipe as mp
 
 mp_face_mesh = mp.solutions.face_mesh
 face_mesh = mp_face_mesh.FaceMesh( static_image_mode=False, max_num_faces = 3 )
+
+# Drawing utils
+map_drawing = mp.solutions.drawing_utils
+
