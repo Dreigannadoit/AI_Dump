90c8d4bf32d1d05d0d3dd8937a6f532048cc4c24
Drei Abmab <dreiabmab@gmail.com>
2025-09-16 22:49:39 +0800

    Set default drawing style

--- Facial_Landscape/main.py
+++ Facial_Landscape/main.py
@@ -8,4 +8,4 @@ face_mesh = mp_face_mesh.FaceMesh( static_image_mode=False, max_num_faces = 3 )
 
 # Drawing utils
 map_drawing = mp.solutions.drawing_utils
-
+mp_drawing_styles = mp.solutions.drawing_styles
