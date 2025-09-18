0800dd1d4d962223ba0c4f195beba76ab373c142
Drei Abmab <dreiabmab@gmail.com>
2025-09-18 21:32:49 +0800

    _

--- Facial_Landscape/main.py
+++ Facial_Landscape/main.py
@@ -5,7 +5,6 @@ import mediapipe as mp
 
 mp_face_mesh = mp.solutions.face_mesh
 face_mesh = mp_face_mesh.FaceMesh( static_image_mode=False, max_num_faces = 3 )
-
 # Drawing utils
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
